967e8bc766642e5c4221b6bbc58616c356c07617
snazzy-nerd <48488361+snazzy-nerd@users.noreply.github.com>
2020-09-11 12:36:25 -0400

    Update models.py

--- orders/models.py
+++ orders/models.py
@@ -6,28 +6,10 @@ from coupons.models import Coupon
 
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    address = models.CharField(max_length=250)
-    phone = models.CharField(max_length=14)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    paid = models.BooleanField(default=False)
     braintree_id = models.CharField(max_length=150, blank=True)
-    coupon = models.ForeignKey(Coupon,related_name='orders',null=True,blank=True,on_delete=models.SET_NULL)
-    discount = models.IntegerField(default=0,validators=[MinValueValidator(0),MaxValueValidator(100)])
-    class Meta:
-        ordering = ('-created',)
 
     def __str__(self):
         return f'Order {self.id}'
-
-    def get_total_cost(self):
-
-        total_cost = sum(item.get_cost() for item in self.items.all())
-        return total_cost - total_cost * (self.discount / Decimal(100))
 
 
 class OrderItem(models.Model):
@@ -37,7 +19,7 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product,
                                 related_name='order_items',
                                 on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
