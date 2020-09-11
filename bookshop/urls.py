175b06525abf312c84fa482e4bd452321af3a914
snazzy-nerd <48488361+snazzy-nerd@users.noreply.github.com>
2020-09-11 12:29:11 -0400

    Update urls.py

--- bookshop/urls.py
+++ bookshop/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.index, name='index'),
     path('product/detail/<slug:slug>', views.product_detail,name='product_detail'),
-    path('product/all-products',views.product_list,name='product_list'),
+    path('',views.product_list,name='product_list'),
     path('category/<slug:category_slug>',views.product_list,name='category_detail'),
     path('all-categories/',views.all_Categories,name='all_categories'),
     path('contact-us/',views.contact_us,name='contact_us'),
